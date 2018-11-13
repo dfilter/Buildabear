b2e1f15b013eda3a619dc2c99cf430d4ee00fb70
dfilter <dirk.b.filter@hotmail.ca>
2018-11-12 21:41:47 -0500

    minor changes

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Database Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -12,6 +14,6 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # JWT
-    JWT_SECRET_KEY = os.urandom(24)
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
