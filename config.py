ad4ba12f3fca60ecb9ee5440f3cc7d3b7a106d5c
dfilter <dirk.b.filter@hotmail.ca>
2018-11-29 15:14:49 -0500

    added SQLALCHEMY_POOL_RECYCLE

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 20
 
     # JWT
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
